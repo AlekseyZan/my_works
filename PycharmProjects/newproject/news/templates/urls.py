ffe6857f3f323bde62a941cf4d73f2392b7636c6
AlekseyZan <121750981+AlekseyZan@users.noreply.github.com>
2023-05-23 10:50:50 +0300

    create new project

---
+++ PycharmProjects/newproject/news/templates/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from news.templates.views import index, get_category
+
+urlpatterns = [
+
+    path('', index, name='Home'),
+    path('category/<int:category_id>', get_category, name='Category')
+
+]
+
---
+++ PycharmProjects/newproject/news/templates/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .models import News, Category
+
+
+def index(request):
+    news = News.objects.all()
+    categories = Category.objects.all()
+
+    context = {'News': news,
+               'title': "Все новости",
+
+               }
+    return render(request, 'News/index.html', context=context)
+
+
+def get_category(request, category_id):
+    news = News.object.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'News': news,
+        'category': category
+    }
+    return render(request, 'News/category.html', context=context)
